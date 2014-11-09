dbd7fa12727db3102f9d7e0fbe085cf251e8ac9a
Eric Daams <eric@164a.com>
2014-11-09 20:11:56 +0930

    Setting up sideways compatibility for ATCF Crowdfunding.

---
+++ includes/atcf-compat/atcf-compat-functions.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Functions that add partial compatibility for Crowdfunding by Astoundify
+ *
+ * @version		1.0.0
+ * @package		EDD Crowdfunding/Functions/ATCF Compatibility
+ * @copyright 	Copyright (c) 2014, Eric Daams	
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @category	Functions
+ * @author 		Studio164a
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function crowdfunding() {
+	_deprecated_function( __FUNCTION__, '1.0.0', 'eddcf()' );
+	return eddcf();
+}
+
+function atcf_purchase_variable_pricing() {
+	/**
+	 * @todo
+	 */
+}
